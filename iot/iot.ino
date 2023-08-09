fa30dad550a06b682bceddddc6b45720b2a2e218
“Fauzan” <apauaie@gmail.com>
2023-08-10 00:06:10 +0800

    update code

--- iot/iot.ino
+++ iot/iot.ino
@@ -1,10 +1,13 @@
-void setup{
-  pinMode(4,OUTPUT);
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop {
-  digitalWrite(4,HIGH);
-  delay(1000);
-  digitalWrite(4,HIGH);
-  delay(1000);
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
